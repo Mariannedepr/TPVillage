efb5fe0b7a69c9c5ba65f880ac27097b40567ad9
Marianne de Prémont <mariannedepremont@gmail.com>
2023-01-12 11:07:49 +0100

    commit EX01

--- Village.cs
+++ Village.cs
@@ -4,13 +4,15 @@ public class Village {
     //comme elle est de type ressource elle peut utiliser les méthodes de la classe Ressource
 
     public House chefHome;    //chefHome peut maintenant utiliser les méthodes de la classe house
-    private int villageois = 10;
+    private int villageois = 0;
+ 
 
 //constructeur de la classe 
     public Village(){
         this.name = name;
         this.myRessources = new Ressources();   //instanciation 
-        this.villageois = villageois;
+        this.chefHome = new House();
+        this.villageois = House.villageois;   //comme mon attribut villageois est public dans ma classe House on peut le call ici
     }
     public string getName(){
         return name;
