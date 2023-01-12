4b56fa52a28c4660eb0cde6e5dd7430b548d2c77
Marianne de Prémont <mariannedepremont@gmail.com>
2023-01-12 14:38:52 +0100

    EXO2

--- Program.cs
+++ Program.cs
@@ -3,8 +3,21 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine(House.wood_needed); // affichera 3
 Console.WriteLine(House.stone_needed); // affichera 3
-// Console.WriteLine(House.stone_needed++) // --> Erreur
-House justAHouse = new House();
-// justAHouse.wood_need // --> Erreur
-// justAHouse.villageois // --> Erreur
-// justAHouse.villageois = 20 // --> Erreur
+
+
+Village myVillage = new Village("Marianne la créatrice du monde");
+myVillage.getName(); // affichera Marianne le createur
+Console.WriteLine(myVillage.getName());
+Console.WriteLine(myVillage.listHouse.Length);
+
+
+myVillage.addHouse(new House());
+myVillage.addHouse(new House());
+myVillage.addHouse(new House());
+myVillage.addHouse(new House());
+myVillage.addHouse(new House());
+Console.WriteLine(myVillage.listHouse.Length); // affichera 3
+
+
+
+Console.WriteLine(myVillage.villageois); // affichera 30
--- Village.cs
+++ Village.cs
@@ -4,18 +4,22 @@ public class Village {
     //comme elle est de type ressource elle peut utiliser les méthodes de la classe Ressource
 
     public House chefHome;    //chefHome peut maintenant utiliser les méthodes de la classe house
-    private int villageois = 0;
- 
+    public int villageois = 0;
+    public House []listHouse;   //c'est un tab
 
 //constructeur de la classe 
-    public Village(){
+    public Village(string name){
         this.name = name;
         this.myRessources = new Ressources();   //instanciation 
         this.chefHome = new House();
         this.villageois = House.villageois;   //comme mon attribut villageois est public dans ma classe House on peut le call ici
+        this.listHouse = new House [1];    
+        this.listHouse[0] = this.chefHome;
+       
     }
+
     public string getName(){
-        return name;
+        return this.name;
     }
 
     public int getWood(){
@@ -26,5 +30,24 @@ public class Village {
         return myRessources.getStone();
     }
 
-    
+//ajout de maisons dans notre tableau listHouse
+    //pour ajouter un élément à un tableau : il faut appeler le tableau
+    //on va donc créer un nouveau tableau qui aurait pour taille le nombre d'éléments
+    //on attribue les éléments de l'ancien tableau (ici la maison du chef)
+    //appeler le tableau pour lui attribuer un nouvel element
+
+    private void addHouse(House maison){
+       House []listHouse2 = new House [listHouse.Length +1];   //on incrémente la taille de notre tableau
+        for(int i = 0; i <listHouse.Length; i++){    //on parcourt tt les indexs de l'ancien tableau
+            listHouse2[i] = listHouse[i];     //on attribue le contenu de l'ancien tableau au nouveau et ce pour toutes les cases de l'ancien tableau
+        }
+
+        listHouse2[listHouse2.Length -1] = maison;    //j'appelle mon nouveau tableau
+        //le -1 il sert à appeler le dernier index de mon tableau 
+
+        //on va réattribuer la valeur de notre nouveau tableau à notre ancien tableau :
+        listHouse = listHouse2;
+        this.villageois = listHouse.Length * House.villageois;
+    }
+
 }
